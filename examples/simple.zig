51fb800f5db6660999600f3db65fa1dab1588f75
Cato Auestad <cato@cato.ninja>
2020-05-18 23:52:08 +0200

    Expanded example with handshake and read

--- examples/simple.zig
+++ examples/simple.zig
@@ -14,14 +14,46 @@ pub fn main() !void {
     try mbed.x509CrtParseFile(cafile);
     try mbed.ctrDrbgSeed("SampleDevice");
     try mbed.netConnect("google.com", "443", mbedTLS.Proto.TCP);
-    try mbed.sslConfigDefaults(.IS_CLIENT, .TCP, .DEFAULT);
+    try mbed.sslConfDefaults(.IS_CLIENT, .TCP, .DEFAULT);
 
     mbed.sslConfAuthmode(.NONE);
     mbed.sslConfRng(null); //use default
-    mbed.setDebug(null); // use default
+    mbed.setConfDebug(null); // use default
+    mbed.sslConfCaChain(null); // use parsed CA file from earlier
 
-    try mbed.setHostname("hello"); // use default
+    try mbed.sslSetup(); // use parsed CA file from earlier
+    try mbed.setHostname("hello");
+    mbed.sslSetBIO();
+
+    var run = true;
+    while(run) {
+        const r: bool = mbed.sslHandshake() catch |err| res: {
+            switch(err) {
+                error.WantRead => break :res false,
+                error.WantWrite => break :res false,
+                else => unreachable
+            }
+        };
+
+        run = !r;
+    }
+
     const req = "GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n";
-    const ret = try mbed.sslWrite(req);
+    var ret: i32 = 0;
+    while(ret <= 0) {
+        ret = try mbed.sslWrite(req);
+    }
+
+    ret = 0;
+    while(true) {
+        var buf: [1024]u8 = undefined;
+        ret = try mbed.sslRead(buf[0..]);
+        if(ret == 0) break;
+        if(ret < 0) break;
+
+        std.debug.warn("Bytes read {}", .{buf});
+        break;
+    }
+
     return;
 }
